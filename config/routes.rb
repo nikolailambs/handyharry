e45151368b372112c0d92812df85d9d4346242ad
nikolai <nikolai.lambsdorff@gmx.de>
2017-11-27 16:26:38 +0100

    setup and half pundit

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   devise_for :clients
   devise_for :handies
 
-  resources :projects do
-    resources :tasks
+
+  namespace :handies do
+    resources :projects do
+      resources :tasks
+    end
+
+    resources :messages, except: [:update, :edit]
   end
 
-  resources :messages, except: [:update, :edit]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
