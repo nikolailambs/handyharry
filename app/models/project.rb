0f60f1436092fd632508d5789d6c2bf420785f9e
Henriette <henriette.fleischmann@gmail.com>
2017-11-27 17:06:51 +0100

    tasks validation added

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :handy
   belongs_to :client
-  has_many :stati, :through => :tasks
   has_many :tasks, inverse_of: :project, dependent: :destroy
   accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
 
