7d774775bd49c8603aade28a19f0bd03d615e3e5
nikolai <nikolai.lambsdorff@gmx.de>
2017-11-28 15:23:07 +0100

    some work in index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:handy])
+  end
 
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,39 @@
 class MessagesController < ApplicationController
+
+
+  def inbox
+    @messages = Message.all.where(sender_id: current_user)
+  end
+
   def index
+    @messages = Message.all.where(sender_id: current_user).where(receiver_id: params[:id])
   end
 
   def show
   end
 
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.sender = current_user
+    @message.receiver = User.find(params[:message][:receiver_id])
+
+    if @message.save
+      redirect_to messages_path
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :content, :sender_id, :receiver_id)
   end
 end
