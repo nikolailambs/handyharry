e222a0a6a5649f6bbc640099c7304d87b3ccdea2
Henriette <henriette.fleischmann@gmail.com>
2017-11-30 20:13:33 +0100

    task checkbox changes status

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.3.5'
 
+gem 'faker'
 gem "pundit"
 gem 'devise'
 gem 'figaro'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,87 +1,108 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or create!d alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first
-
-require 'faker'
+#   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create!(name: 'Luke', movie: movies.first
 
 User.destroy_all
 Project.destroy_all
 Task.destroy_all
 
-@user_ids = (1..10).to_a
-@date = rand(1.year).seconds.from_now
 
-@project_ids =(1..5).to_a
+client_one = User.create!(
+  :email => "client@user.de",
+  :password => "123456",
+  :handy => false,
+)
 
+client_two = User.create!(
+  :email => "client2@user.de",
+  :password => "123456",
+  :handy => false,
+)
 
-puts 'Creating 5 fake users...'
+handy = User.create!(
+  :email => "handy@user.de",
+  :password => "123456",
+  :handy => true,
+)
 
-5.times do
-  user = User.new(
-    email: Faker::Internet.email,
-    password: "123456",
-    speciality: "",
-    handy: false,
-    first_name: Faker::Name.first_name,
-    second_name: Faker::Name.last_name,
-    avatar: "/assets/images/client.png",
-    phone: "",
-    status: ""
+p1= Project.create!(
+  :address => "Berliner Allee",
+  :status => false,
+  :description => "Repair Toilet",
+  :title => "Toilet",
+  :client_id => client_one.id,
+  :handy_id => handy.id,
   )
-  user.save!
-end
 
-puts 'Creating 5 fake handies...'
-
-5.times do
-  user = User.new(
-    email: Faker::Internet.email,
-    password: "123456",
-    speciality: "",
-    handy: true,
-    first_name: Faker::Company.name,
-    second_name: Faker::Name.last_name,
-    avatar: "/assets/images/handy.png",
-    phone: "",
-    status: ""
+p2 = Project.create!(
+  :address => "Frankfurter Allee",
+  :status => false,
+  :description => "Set up kitchen",
+  :title => "Kitchen",
+  :client_id => client_two.id,
+  :handy_id => handy.id,
   )
-  user.save!
-end
 
-puts 'Creating 2 fake projects...'
-
-5.times do
-  project = Project.new(
-    handy_id: @user_ids.sample,
-    client_id: @user_ids.sample,
-    address: Faker::Address.street_address,
-    deadline: @date,
-    status: false,
-    description: "Toilet is broken. Please fix the toilet seat",
-    title: "Toilet",
-    location: Faker::Address.secondary_address
+t1 = Task.create!(
+  :description => "abcdefgh",
+  :status => false,
+  :project_id => p1.id
   )
-  project.save!
-end
 
-puts 'Creating 2 fake tasks...'
-
-2.times do
-  task = Task.new(
-    project_id: @project_ids.sample,
-    title: "",
-    description: "Buy toilet seat.",
-    assigned_to: "",
-    status: false,
-    deadline: @date
+t2 = Task.create!(
+  :description => "jhagdfjhlafgsdhj",
+  :status => false,
+  :project_id => p1.id
   )
-  task.save!
-end
 
-puts 'Finished!'
+t3 = Task.create!(
+  :description => "lkahflk",
+  :status => true,
+  :project_id => p1.id
+  )
 
+t4 = Task.create!(
+  :description => "a,jbdakjs",
+  :status => true,
+  :project_id => p1.id
+  )
 
+t5 = Task.create!(
+  :description => "kahdflksh",
+  :status => false,
+  :project_id => p1.id
+  )
+
+t1 = Task.create!(
+  :description => "abcdefgh",
+  :status => false,
+  :project_id => p2.id
+  )
+
+t2 = Task.create!(
+  :description => "jhagdfjhlafgsdhj",
+  :status => true,
+  :project_id => p2.id,
+  )
+
+t3 = Task.create!(
+  :description => "lkahflk",
+  :status => true,
+  :project_id => p2.id
+  )
+
+t4 = Task.create!(
+  :description => "a,jbdakjs",
+  :status => true,
+  :project_id => p2.id,
+  )
+
+t5 = Task.create!(
+  :description => "kahdflksh",
+  :status => false,
+  :project_id => p2.id,
+  )
