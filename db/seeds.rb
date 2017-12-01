e754241b9ce87e9cd6ae1735d6e025c4019f3ec6
Christophe Vercarre <christophe.vercarre@gmail.com>
2017-12-01 11:56:32 +0100

    Added customers to mobile version/Changed the z-index of active card-links/Made a button for customers link

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-Rails.env.development?
-  User.destroy_all
-  Project.destroy_all
-  Task.destroy_all
-end
+# if Rails.env.development?
+#   Chat_room.destroy_all
+#   Message.destroy_all
+#   User.destroy_all
+#   Project.destroy_all
+#   Task.destroy_all
+# end
 
 @user_ids = (1..10).to_a
 @date = rand(1.year).seconds.from_now
