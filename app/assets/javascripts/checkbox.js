c2b4794add2df1ee5a23b75d101ec1d4d4be84b8
Henriette <henriette.fleischmann@gmail.com>
2017-12-01 13:50:22 +0100

    status bar working, style shity

--- app/assets/javascripts/checkbox.js
+++ app/assets/javascripts/checkbox.js
@@ -1,5 +1,8 @@
-$('input.task-check').change(function(){
+$('input.task-check').change(function method1(){
   var task_id = $(this).data('task-id')
   console.log('form_' + task_id)
   $('#form_' + task_id).submit()
 })
+
+
+
