116f908fe6e4da412464ec8bbd53817980d094df
Henriette <henriette.fleischmann@gmail.com>
2017-12-04 16:44:25 +0100

    set up FB integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :projects do
     resources :tasks, only: [:new, :create, :destroy, :update, :edit]
