fd73e294b3ea2b6cd8a7da5d3028d834b933e62a
Christophe Vercarre <christophe.vercarre@gmail.com>
2017-12-05 14:33:18 +0100

    Updated the project show page. update and create.js

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,6 @@ module Handyharry
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
